4b696ec71d1cb86ad009eec04d8ae2e43bc8220e
tuks <joris@placette.fr>
2017-04-05 09:55:43 +0200

    aut

--- client.py
+++ client.py
@@ -2,6 +2,7 @@
 # -∗- coding: utf-8 -∗-
 import socket # on importe le module
 import threading
+
 global Sock
 class Net ():
     def __init__(self,Host, Port, Nickname, Pass):
@@ -10,26 +11,13 @@ class Net ():
         self.Nickname = Nickname
         self.Pass = Pass
         self.Sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # on cree notre socket
-        self.Sock.connect((self.Host,self.Port))
-    def Connect(self):
+        self.Sock.connect((self.Host,self.Port)) # on se connecte sur le serveur avec les informations données
 
-        self.Sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # on cree notre socket
-
-
-        try:
-            # on se connecte sur le serveur avec les informations ci-dessus
-            # assurez-vous d'avoir mis en marche le serveur !
-            2*22
-        except:
-            print("connection au serveur {}:{} impossible".format(Host, Port))
-
-    def SendMsgLoop(self):
-        while 1:
-
-            msg = bytes(input('{}>> '.format(Host)), 'utf8') # on rentre des donnees
-            self.Sock.send(msg) # on envoie ces donnees
-
-
+    def Authenticate(self):
+        data = bytes("AUT" +data)
+    def SendMsgLoop(self, msg):
+        data = bytes(msg, 'utf8') # on rentre des donnees
+        self.Sock.send(data) # on envoie ces donnees
 
 ##phase de login
 Host =input("Entrez l'ip du serveur auquel se conecter; entrer '0' pour '127.0.0.1': ")
@@ -40,7 +28,7 @@ Nickname = input("Saisissez un Pseudo: ")
 Pass = input("Saisissez un mot de passe: ")
 
 MyNet = Net(Host, Port , Nickname, Pass)
-#MyNet.Connect()
-MyNet.SendMsgLoop()
+MyNet.Authenticate()
+MyNet.SendMsgLoop('print("couocu")')
 
 # On est connecte, on fait une boucle infinie d'inputs pour l'envoi des messages :
