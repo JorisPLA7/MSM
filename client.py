4ec153ebccfca60e0dc08ea56d0a29cd3715bce4
tuks <joris@placette.fr>
2017-04-09 14:40:04 +0200

    Identification des clients connectés par pseudo
    pseudos fournis par le cilent lui-même.

--- client.py
+++ client.py
@@ -9,32 +9,41 @@ class Net ():
         self.Host = socket.gethostbyname(Host)
         self.Port = Port
         self.Nickname = Nickname
+        self.NickLen = str(len(self.Nickname))
         self.Pass = Pass
         self.Sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # on cree notre socket
-        self.Sock.connect((self.Host,self.Port)) # on se connecte sur le serveur avec les informations données
 
     def Authenticate(self):
-        data = bytes("AUT" +self.Nickname)
-        self.Sock.send(data, flags=None)
-    def SendMsgLoop(self, msg):
+        data = bytes("AUT" + self.NickLen + self.Nickname, 'utf8')
+        self.Sock.connect((self.Host,self.Port)) # on se connecte sur le serveur avec les informations données
+        self.Sock.send(data)
+
+    def Disconnect(self):
+        print("Disconnected", sep=' ')
+
+    def SendMsg(self, msg):
         data = bytes(msg, 'utf8') # on rentre des donnees
         self.Sock.send(data) # on envoie ces donnees
 
     def WhoAmI(self):
         return self.Nickname
+    
+def login(): 
+    ##phase de login
+    Host = "0"
+    if Host == "0":
+            Host ="127.0.0.1"
+    Port = 8082
+    Nickname = "coucoué"
+    Pass = "lol ;')"
 
-##phase de login
-Host =input("Entrez l'ip du serveur auquel se conecter; entrer '0' pour '127.0.0.1': ")
-if Host == "0":
-        Host ="127.0.0.1"
-Port = 8082
-Nickname = input("Saisissez un Pseudo: ")
-Pass = input("Saisissez un mot de passe: ")
+    MyNet = Net(Host, Port , Nickname, Pass)
+    MyNet.Authenticate()
+    MyNet.SendMsg('print("couocu")')
+    print(MyNet.WhoAmI())
+    a = input()
 
-MyNet = Net(Host, Port , Nickname, Pass)
-#MyNet.Authenticate()
-MyNet.SendMsgLoop('print("couocu")')
-MyNet.WhoAmI()
-a = input()
+while 1 :
+    login()
 
 # On est connecte, on fait une boucle infinie d'inputs pour l'envoi des messages :
--- customServer0.2.py
+++ customServer0.2.py
@@ -10,43 +10,86 @@ Port = 8082 # choix d'un port
 
 # on bind notre socket :
 Sock.bind((Host,Port))
+class ServerNet():
+    def __init__(self):
+        self.ReceptionistThread = Receptionist(1, "ReceptionistThread")
 
-class Listener (threading.Thread):
+    def Listen(self,Toogler):
+        if Toogler == True:
+            self.ReceptionistThread.start()
+        #if Toogler == False:
+            #self.ReceptionistThread.Stop() #non fonctionnel
+
+
+class Receptionist (threading.Thread):
     def __init__(self, threadID, name):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
+        self.ClientList = {}
     def run(self):
         i = 0 # i : thread counter
-        while 1:# On est a l'ecoute d'une seule et unique connexion :
+        while 1:# On est a l'ecoute d'une seule et unique connexion à la fois :
             Sock.listen(2)
             # Le script se stoppe ici jusqu'a ce qu'il y ait connexion :
-            client, address = Sock.accept() # accepte les connexions de l'exterieur
-            HandlerThread = Handler(i, client, address)
+            Client, Address = Sock.accept() # accepte les connexions de l'exterieur
+            HandlerThread = Handler(i, Client, Address)
             HandlerThread.start()
+            self.ClientList[i] = [i, Client, Address]
             i+=1
-class Handler (threading.Thread): # conserve un lien avec le client
-    def __init__(self, threadID, client, address):
+    #def Stop(self):
+            #Sock.shutdown(SHUT_RDWR)
+            #Sock.close(RDWR)
+
+class Handler (threading.Thread): # conserve un lien avec le Client
+    def __init__(self, threadID, Client, Address):
         threading.Thread.__init__(self)
         self.threadID = threadID
-        self.client = client
-        self.address = address
+        self.Client = Client
+        self.Address = Address
+        self.Authenticated = False
+        self.Nickname = None
+        self.NickLen = None
     def run(self):
-        print(("L'address {} vient de se connecter au serveur !").format(self.address))
+        while not self.Authenticated:
+            data = self.Client.recv(512) # on recoit x caracteres grand max
+            RequeteDuClient = data.decode()
+            RequeteDuClient = str(object=RequeteDuClient)
+            print(RequeteDuClient)
+            print(type(RequeteDuClient))
+            print(RequeteDuClient[0:3])
+            if RequeteDuClient[0:3] == 'AUT':
+                NickLen = int(RequeteDuClient[3])
+                print(NickLen)
+                self.Nickname = RequeteDuClient[4:4+NickLen]
+                self.Authenticated = True
+                print("Client {} authentifié \naddresse : {}".format(self.Nickname, self.Address))
+                #print(self.ClientList, sep=' ', end='n', file=sys.stdout, flush=False)
+
+
         while 1:
             try:
-                RequeteDuClient = self.client.recv(1024) # on recoit 255 caracteres grand max
+                RequeteDuClient = self.Client.recv(1024) # on recoit 255 caracteres grand max
                 if not RequeteDuClient: # si on ne recoit plus rien
-                    print(("L'address {} vient de se déconnecter!").format(self.address))
+                    print(("L'adresse {} vient de se déconnecter!").format(self.Address))
                     break  # on break la boucle (sinon les bips vont se repeter)
                 try:
                     exec(RequeteDuClient.decode())# affiche les donnees
                 except:
-                    print("LOG: Commande rendeignée par {} impossible ('{}')" .format(self.address, RequeteDuClient))
+                    print("LOG: Commande rendeignée par {} impossible ('{}')" .format(self.Address, RequeteDuClient))
             except:
-                print("Le client {} s'est déconnecté".format(self.address))
+                print("Le Client {} s'est déconnecté".format(self.Address))
                 break
 
+MyServ = ServerNet()
+MyServ.Listen(True)
+print("En attente de clients...")
 
-ListenerThread = Listener(1, "ListenerThread")
-ListenerThread.start()
+
+'''    if not RequeteDuClient: # si on ne recoit plus rien
+        print(("L'Address {} vient de se déconnecter!").format(self.Address))
+        break  # on break la boucle (sinon les bips vont se repeter)
+        print(("L'Address {} vient de se connecter au serveur !").format(self.Address))
+
+
+'''
