0ee8ebaddbb0f3e085dc5f0290f73db54496bf66
arthur-du <arthurduca38@gmail.com>
2017-05-10 08:30:36 +0200

    Add files via upload

--- premiere page.py
+++ premiere page.py
@@ -6,9 +6,7 @@ mdptest='123'
 
 def go():
    a=pseudo.get()
-   b=ip.get()
-   c=mdp2.get()
-   if a==pseudotest and c==mdptest:
+   if a!='':
       fenetre.destroy()
    else:
       messagebox.showinfo("ERREUR", "Il semble que votre pseudo ou mot de passe soit incorrect.")
@@ -23,13 +21,6 @@ bsal = Frame(fenetre)
 bsal.pack(side=BOTTOM)
 bgo = Button(bsal, text ="GO", command = go, anchor=CENTER, pady=4, height=3, width=15)
 bgo.pack(side = BOTTOM)
-##ip
-ipsal = Frame(fenetre)
-ipsal.pack()
-ipn = Label(ipsal, text="Ip:", pady=4, padx=16)
-ipn.pack(side = LEFT)
-ip = Entry(ipsal)
-ip.pack(side=RIGHT)
 ##pseudo
 pseudal = Frame(fenetre)
 pseudal.pack()
@@ -37,13 +28,6 @@ psn = Label(pseudal, text="Pseudo:", pady=4)
 psn.pack(side = LEFT)
 pseudo = Entry(pseudal)
 pseudo.pack(side = RIGHT)
-##mdp
-mdp = Frame(fenetre)
-mdp.pack()
-mdp1 = Label(mdp, text="Mot de passe:", pady=4)
-mdp1.pack(side = LEFT)
-mdp2 = Entry(mdp, show='*')
-mdp2.pack(side = RIGHT)
 ##verif pseudo/mdp
 
 
